fa3605bcc8878aaba273df2277cb1df67ec27309
wangshiyu <shiyu.wang.ui@gmail.com>
2022-07-20 15:52:11 +0800

    feat: make basic ui layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
+import { Form, Input, InputNumber, Button, Typography } from "@arco-design/web-react";
+import "@arco-design/web-react/dist/css/arco.css";
 import './App.css';
 
+const FormItem = Form.Item;
+
 function App() {
+  const [form] = Form.useForm();
+
+  const onSubmit = () => {
+    form.validate(async (errors, values) => {
+      if (!errors) {
+        console.log('values', values);
+      }
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography.Title>Transfer Your Token Here</Typography.Title>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Form style={{ width: 600 }} form={form} onSubmit={onSubmit}>
+          <FormItem label='Address' field='address' rules={[{ required: true, message: 'Please enter' }]}>
+            <Input placeholder='Recipient Address...' />
+          </FormItem>
+          <FormItem
+            label='Amount'
+            field='amount'
+            rules={[{ required: true, type: 'number', min: 0 }]}
+          >
+            <InputNumber placeholder='Make sure you have IYO token...' />
+          </FormItem>
+          <FormItem>
+            <Button type='primary' htmlType='submit'>Submit</Button>
+          </FormItem>
+        </Form>
+      </div>
     </div>
   );
 }
